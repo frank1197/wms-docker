237905c6d04b6b76b6ea376c2066b23a4c5e4f6a
yanf <yanf@xx.com>
2026-01-01 23:09:04 +0800

    新增personnel_IO,以及对应双天线

--- mysql/init/FACE_PIC_SEGMENT_INFO.sql
+++ mysql/init/FACE_PIC_SEGMENT_INFO.sql
@@ -22,9 +22,9 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `face_pic_segment_info`;
 CREATE TABLE `face_pic_segment_info`  (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `fileid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `segmentidx` int(11) NULL DEFAULT NULL,
   `segmenttotal` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
